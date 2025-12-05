44fae6d61a16e2e97304e28cfabd57525e93dc57
pietro-giordano-algomera <pietro.giordano@algomera.it>
2025-12-05 12:49:58 +0100

    permanent redirect to login page and removed welcome template page

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::permanentRedirect('/', 'login');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
